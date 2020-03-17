435ebd7e14246174bf75e7dbbed8a4831f1f8879
Kyle Talmage <ktalmage85@gmail.com>
2020-03-17 12:55:46 +0000

    Automatically backed up by Learn

--- db/migrate/003_create_haunted_houses.rb
+++ db/migrate/003_create_haunted_houses.rb
@@ -1,3 +1,14 @@
 CreateHauntedHouses < ActiveRecord::Migration[5.1]
+ def change
+    create_table :haunted_houses do |t|
+      t.string :name
+      t.string :location
+      t.string :theme
+      t.boolean :family_friendly
+      t.timestamp :opening_date
+      t.timestamp :closing_date
+      t.description :long_description
+    end
+  end
 end
 # Create your haunted_houses migration here
